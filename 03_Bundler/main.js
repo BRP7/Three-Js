f107e458df909c35412385cd4aeac95b187973a9
BRP7 <bhumiparmar0007@gmail.com>
2024-09-29 21:16:29 +0530

    Orbit Control

--- 03_Bundler/main.js
+++ 03_Bundler/main.js
@@ -1,12 +1,14 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 , wireframe : true } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+
 
 camera.position.z = 5;
 
@@ -19,12 +21,14 @@ window.addEventListener('resize',()=>{
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 })
+const controls = new OrbitControls(camera,renderer.domElement)
 
 function animate() {
-    renderer.requestAnimationFrame( animate );
+    requestAnimationFrame( animate );
 	renderer.render( scene, camera );
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    controls.update();
 }
 
 animate()
