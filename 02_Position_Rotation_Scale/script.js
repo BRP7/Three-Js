451a60402b0ea92b565423b035d1c60bf2d70dfc
BRP7 <bhumiparmar0007@gmail.com>
2024-09-29 20:24:46 +0530

    Clock setting in 3js

--- 02_Position_Rotation_Scale/script.js
+++ 02_Position_Rotation_Scale/script.js
@@ -17,26 +17,26 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( box, material );
 
 // cube.position.x = 2;//If the camera is at `z = 5` and the cube is at `x = 10`, the cube won't be visible because it's outside the camera's frustum.
-cube.position.x = -2;//the last position set on an axis will take effect.
-cube.position.y = 1;
-cube.position.y = -1;
-cube.position.z = 1;
+// cube.position.x = -2;//the last position set on an axis will take effect.
+// cube.position.y = 1;
+// cube.position.y = -1;
+// cube.position.z = 1;
 
 cube.rotation.x= 1;
-cube.rotation.y= 2;
-cube.rotation.y= Math.PI;//180 degree
-cube.rotation.y= Math.PI*2;
-cube.rotation.z= -3;
+// cube.rotation.y= 2;
+// cube.rotation.y= Math.PI;//180 degree
+// cube.rotation.y= Math.PI*2;
+// cube.rotation.z= -3;
 
-cube.scale.x= -1.1;
-cube.scale.y= 2.2;
-cube.scale.z= 4;
+// cube.scale.x= -1.1;
+// cube.scale.y= 2.2;
+// cube.scale.z= 4;
 
 scene.add( cube );
 
 
 let canvas = document.querySelector('#draw');
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas ,antialias :true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.render(scene,camera);
 
@@ -46,4 +46,12 @@ window.addEventListener("resize",()=>{
     camera.updateProjectionMatrix();
 })
 
-
+let clock = new THREE.Clock();
+function animation(){
+    window.requestAnimationFrame(animation);
+    renderer.render(scene,camera);
+    // cube.rotation.y += 0.01;//depends upon computer FPS
+    cube.rotation.y = clock.getElapsedTime() ;
+    cube.rotation.y = clock.getElapsedTime() * 4;
+}
+ animation();
