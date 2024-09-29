e4e4e4c0583909f6bf080b287bd1cdedfdf6f341
BRP7 <bhumiparmar0007@gmail.com>
2024-09-29 21:30:46 +0530

    Sphere geometry

--- 03_Bundler/main.js
+++ 03_Bundler/main.js
@@ -4,7 +4,9 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const geometry = new THREE.SphereGeometry( 1, 10, 10 );
+const geometry = new THREE.SphereGeometry( 1, 5, 5 ,2,1.2);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 , wireframe : true } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
